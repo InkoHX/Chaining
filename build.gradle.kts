64cd6a75a7958a300a46f939bc3e0e24aa396c5f
InkoHX <me@inkohx.dev>
2021-07-06 14:37:05 +0900

    build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("com.github.johnrengelman.shadow") version "7.0.0"
+    kotlin("jvm") version "1.5.20"
+}
+
+group = "dev.inkohx.papermc.chaining"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+    maven("https://papermc.io/repo/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("com.destroystokyo.paper:paper-api:1.16.5-R0.1-SNAPSHOT")
+}
+
+tasks {
+    shadowJar {
+        archiveBaseName.set("Chaining")
+        archiveClassifier.set("")
+        minimize()
+    }
+    build {
+        dependsOn(shadowJar)
+    }
+}
+
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "16"
+}
+
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "16"
+}
